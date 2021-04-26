03bd851ead970eb0a611e4d8eb5149db0a773e45
han-jungwon <rhehf1024@naver.com>
2021-04-26 10:05:30 +0900

    마지막

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -6,11 +6,13 @@
 </template>
 
 <script>
+import {mapMutations} from "vuex";
+
 export default {
   methods: {
-    clearTodo() {
-      this.$store.commit('clearAllItems');
-    }
+    ...mapMutations({
+      clearTodo: 'clearAllItems'
+    })
   }
 }
 
